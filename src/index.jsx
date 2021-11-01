56d47b5e0a3c5c327ead1efb682e2988307ba46f
myesn <myesn@foxmail.com>
2021-11-01 16:34:39 +0800

    ✨ add and configure redux-persist

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
-import configureStore from './store';
+import { configureStore, configurePersistor } from './store';
 import Router from './router';
 
 import './index.scss';
 
 const store = configureStore();
+const persistor = configurePersistor(store);
 
 ReactDOM.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <Router />
+    <PersistGate persistor={persistor}>
+      <Router />
+    </PersistGate>
   </Provider>,
   // </React.StrictMode>,
   document.getElementById('root')
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers';
 
-const configureStore = () =>
-  createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)));
+const persistConfig = {
+  key: 'foodie',
+  storage,
+  whitelist: ['user'], // user 会存入缓存
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const configureStore = () =>
+  createStore(
+    persistedReducer,
+    composeWithDevTools(applyMiddleware(logger, thunk))
+  );
 
-export default configureStore;
+export const configurePersistor = (store) => persistStore(store);
