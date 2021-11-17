ad1d3e987912f901d9b2a6904b15d5e8ad501374
myesn <myesn@foxmail.com>
2021-11-17 11:36:54 +0800

    🔥 remove unused code

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,5 @@
 // import { lazy } from 'react';
-import { BrowserRouter, Switch, Routes , Route } from 'react-router-dom';
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
 
 import App from '../pages/App';
 import Home from '../pages/Home';
