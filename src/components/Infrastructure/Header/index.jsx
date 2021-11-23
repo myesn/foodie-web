3b58f31a0e918a77265579fee9812f3b5c85b1b4
myesn <myesn@foxmail.com>
2021-11-23 09:54:47 +0800

    ✨ add poem to Infrastructure.Header

--- src/components/Infrastructure/Header/index.jsx
+++ src/components/Infrastructure/Header/index.jsx
@@ -1,7 +1,9 @@
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Layout, Nav } from '@douyinfe/semi-ui';
 import { IconSemiLogo } from '@douyinfe/semi-icons';
+import { load } from 'jinrishici';
 
 import LoggedInAvatar from './LoggedInAvatar';
 import SigninButton from './SigninButton';
@@ -13,8 +15,19 @@ const getUserButtonView = (isAuthenticated) => {
 
 const Header = () => {
   const navigate = useNavigate();
+  const [poem, setPoem] = useState('');
   const isAuthenticated = useSelector(selectIsAuthenticated);
   const userButtonView = getUserButtonView(isAuthenticated);
+
+  const retriveJinRiShiCi = () => {
+    load((result) => {
+      setPoem(result.data.content);
+    });
+  };
+
+  useEffect(() => {
+    retriveJinRiShiCi();
+  }, []);
 
   return (
     <Layout.Header style={{ backgroundColor: 'var(--semi-color-bg-1)' }}>
@@ -25,6 +38,7 @@ const Header = () => {
             onClick={() => navigate('/')}
           />
         </Nav.Header>
+        <Nav.Item onClick={retriveJinRiShiCi}>{poem}</Nav.Item>
         <Nav.Footer style={{ marginRight: 10 }}>{userButtonView}</Nav.Footer>
       </Nav>
     </Layout.Header>
